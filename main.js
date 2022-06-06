0dbe1bae653ef3f7aae4bc01e483dfa38691c518
Sabonare <henrique.sabonare@capgemini.com>
2022-06-06 20:56:20 -0300

    SwitcherTheme RocketSeatProject

---
+++ main.js
@@ -0,0 +1,11 @@
+changeTheme()
+function changeTheme(){
+  let checkbox = document.getElementById("checkbox")
+
+
+  checkbox.addEventListener("change", () => {
+    document.body.classList.toggle("dark")
+  })
+
+  
+}
